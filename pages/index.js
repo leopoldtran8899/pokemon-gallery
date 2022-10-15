aa4b376e53b405a921f8609b6f6e32850108b372
Leopold Tran <leopoldtran8899@gmail.com>
2022-10-15 17:25:47 +1100

    replace with magikarp

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import Button from '../components/Button'
 import PokémonInfoCard from '../components/PokemonInfoCard'
+import Magikarp from '../public/magikarp.png'
 import { getPokemonById } from '../lib/pokemon'
 // import styles from '../styles/Home.module.css'
 
@@ -17,31 +17,38 @@ export default function Home({pokemon1}) {
         <div className='p-2 text-2xl text-center text-transparent uppercase bg-center bg-no-repeat bg-cover md:text-3xl font-display bg-gradient-to-r from-red-500 to-teal-500 bg-clip-text shadow-burn'>
           Pokémon gallery
         </div>
-        <div className=''>
-          <PokémonInfoCard pokemon={pokemon1} />
+        <div className='relative h-48'>
+          <Image
+            src={Magikarp}
+            alt='Picture of the Pokemon'
+            layout='fill'
+            objectFit='cover'
+          />
+          {/* <PokémonInfoCard pokemon={pokemon1} /> */}
           {/* <PokémonInfoCard pokemon={pokemon2} />
         <PokémonInfoCard pokemon={pokemon3} />
         <PokémonInfoCard pokemon={pokemon4} /> */}
         </div>
-        <Button linkTo={'/pokemon'} name='Browse Pokémons'/>
-        <Button linkTo={'/pokemon'} name='Something else'/>
+        <Button linkTo={'/pokemon'} name='Browse Pokémons' />
+        <Button linkTo={'/pokemon'} name='Something else' />
       </div>
     </div>
   );
 }
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const pokemon1 = await getPokemonById(1)
-  // const pokemon2 = await getPokemonById(4)
-  // const pokemon3 = await getPokemonById(7)
-  // const pokemon4 = await getPokemonById(16)
+// export async function getStaticProps() {
+//   // Call an external API endpoint to get posts.
+//   // You can use any data fetching library
+//   const pokemon1 = await getPokemonById(1)
+//   const pokemon2 = await getPokemonById(4)
+//   const pokemon3 = await getPokemonById(7)
+//   const pokemon4 = await getPokemonById(16)
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      pokemon1
-    },
-  }
-}
+//   // By returning { props: { posts } }, the Blog component
+//   // will receive `posts` as a prop at build time
+//   return {
+//     props: {
+//       pokemon1,
+//       pokemons: [pokemon1, pokemon2, pokemon3, pokemon4]
+//     },
+//   }
+// }
