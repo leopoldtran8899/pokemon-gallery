6ba6ba997b3daeaf61100f841099d801659822b5
Leopold Tran <leopoldtran8899@gmail.com>
2022-10-23 19:39:26 +1100

    fix error page

--- pages/Error.js
+++ pages/Error.js
@@ -1,20 +1,8 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-
-export default class Error extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>Hello</div>
-    )
-  }
+const Error = () => {
+  return(
+    <div>Page not found</div>
+  )
 }
-
-Error.propTypes = {
-}
-
+export default Error
 
 
